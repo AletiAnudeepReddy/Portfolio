7c035b3f5727e780d3946ed26b54e001bac431f7
anudeep096 <aletianudeepreddy8@gmail.com>
2025-08-23 18:37:59 +0530

    tape and project

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,11 @@
 import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import { Butterfly_Kids } from "next/font/google";
+import Image from "next/image";
+import { CheckCircleIcon } from "lucide-react";
+import { ArrowUpRightIcon } from "lucide-react";
+import grainImage from "@/assets/images/grain.jpg";
 
 const portfolioProjects = [
   {
@@ -42,5 +47,83 @@ const portfolioProjects = [
 ];
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
+  return (
+    <section className="pb-16 lg:py-24">
+      <div className="container">
+        <div className="flex items-center justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-center bg-clip-text text-transparent">
+            Real-World Results
+          </p>
+        </div>
+
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+          Featured Projects
+        </h2>
+        <p className="text-center text-white/60 md:text-lg lg:text-xl max-w-md mx-auto mt-4">
+          See how I transformed concepts into engaging digital experiences.
+        </p>
+        <div className="flex flex-col gap-20 md:mt-20 mt-10">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="bg-gray-800 rounded-3xl relative overflow-hidden 
+    after:block after:content-[''] after:absolute after:inset-0 
+    after:rounded-3xl after:border-2 after:border-white/30 
+    after:pointer-events-none after:z-20 px-8 pt-8 md:pt-12 md:px-10
+    lg:px-20 lg:pt-16"
+            >
+              <div
+                className="absolute inset-0 -z-10 opacity-5"
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+                <div className="lg:pb-16">
+                  <div
+                    className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase
+                tracking-widest text-sm text-transparent bg-clip-text"
+                  >
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+
+                  <h3 className="font-serif text-2xl md:text-4xl md:mt-5 mt-2">
+                    {project.title}
+                  </h3>
+                  <hr className="border-t-2 border-white/5 md:mt-5 mt-4" />
+                  <ul className="flex flex-col mt-4 gap-4 md:mt-5">
+                    {project.results.map((result) => (
+                      <li className="flex gap-2 text-sm md:text-base text-white/50">
+                        <CheckCircleIcon className="size-4 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button
+                      className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold
+                inline-flex items-center justify-center gap-2 mt-8"
+                    >
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon className="size-4" />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full
+                    lg:w-auto lg:max-w-none"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
